ed7ec816a07e4a52168485757aab85f1f0d40c61
Maorshl <maors@moveo.co.il>
2021-10-28 10:45:14 +0300

    Added to gameOver screen

--- components/screens/GameOver.tsx
+++ components/screens/GameOver.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Image } from "react-native";
 import Card from "../Card";
 
 interface Props {
@@ -9,16 +9,40 @@ interface Props {
 
 const GameOver = ({ numberOfRounds, restartGame }: Props) => {
   return (
-    <Card>
+    <Card style={styles.gameOverCard}>
       <View>
-        <Text>Game Over!</Text>
-        <Text>The Game Took {numberOfRounds} Rounds</Text>
+        <Text style={styles.title}>Game Over!</Text>
+        <View>
+          <Image
+            source={{
+              uri: "https://upload.wikimedia.org/wikipedia/commons/9/95/Kan%C5%8D_Jigor%C5%8D_c1937.jpg",
+            }}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.message}>
+          The Game Took {numberOfRounds} Rounds
+        </Text>
         <Button title="Play Again!" onPress={() => restartGame()} />
       </View>
     </Card>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  gameOverCard: {},
+  title: {
+    alignSelf: "center",
+  },
+  image: {
+    height: 400,
+    width: 300,
+    borderRadius: 200,
+    marginVertical: 20,
+  },
+  message: {
+    alignSelf: "center",
+  },
+});
 
 export default GameOver;
